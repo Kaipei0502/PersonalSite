b4034f9294eadf48642038fa09b68f84d86dd5bd
Kaipei0502 <51125katty@gmail.com>
2026-04-24 22:38:51 +0800

    chore: 初始化前端專案

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // 前端呼叫 /api/xxx 會自動轉發到後端
+      "/api": {
+        target: "https://localhost:44366",
+        changeOrigin: true,
+        secure: false, // 後端是 https localhost 通常要 false
+      },
+    },
+  },
+})
